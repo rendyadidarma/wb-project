ed608d0fbfc27e1c361820594604668a6d999098
yedeneer <69148374+rendyadidarma@users.noreply.github.com>
2021-06-03 20:59:04 +0700

    Add files via upload
    Data Set Source : Keggle

---
+++ book_genre/scripts/products.js
@@ -0,0 +1,27 @@
+// var products = [
+//   {name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/5098/9781509858637", title: "Romance Book 1"},
+//   {name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/1410/9780141033570", title: "Romance Book 2"},
+//   {name: "romance", title: "Romance Book 3"},
+//   {name: "romance", title: "Romance Book 4"},
+
+
+var products =[
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/5098/9781509858637", title: "This is Going to Hurt,Adam Kay"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/1410/9780141033570", title: "Thinking, Fast and Slow"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/7847/9781784701994", title: "When Breath Becomes Air"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/8452/9781845298258", title: "The Happiness Trap,Russ Harris"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/8460/9781846041242", title: "Man's Search For Meaning"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/3305/9780330523622", title: "The Man Who Mistook His Wife for a Hat"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/7807/9781780722405", title: "The 8-week Blood Sugar Diet"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/7807/9781780723044", title: "The Clever Guts Diet"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/2413/9780241351642", title: "12 Rules for Life"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/8466/9781846685828", title: "Being Mortal"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/9252/9781925228601", title: "Gut"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/1410/9780141038872", title: "The Brain That Changes Itself"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/4019/9781401954406", title: "Medical Medium Liver Rescue"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/1419/9780141976143", title: "Wreck This Journal,Keri Smith"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/2413/9780241351635", title: "12 Rules for Life"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/9548/9780954852023", title: "Gut and Psychology Syndrome"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9781/4019/9781401948368", title: "Medical Medium Thyroid Healing"},
+{name: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/7611/9780761169253", title: "Steal Like an Artist"},
+]
